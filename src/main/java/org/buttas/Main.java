c34ccb6120b2e29147070e30c0abe8225de56995
Sushant Butta <sushant.butta@gmail.com>
2024-05-14 22:34:28 +0530

    Add commit

--- src/main/java/org/buttas/Main.java
+++ src/main/java/org/buttas/Main.java
@@ -1,8 +1,16 @@
 package org.buttas;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+        System.out.println("Programme to add 3 numbers");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum of 2 numbers is : " + (a+b));
     }
 }
